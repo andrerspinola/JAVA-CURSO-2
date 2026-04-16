cf906317680a0aedb077ac7966eef57586b5245b
andre <andrecastor828@gmail.com>
2026-04-16 10:12:27 -0300

    loops externor e interno

--- src/secao12/LabelsLoops.java
+++ src/secao12/LabelsLoops.java
@@ -3,5 +3,21 @@ package secao12;
 public class LabelsLoops {
     static void main() {
 
+        // 8 - Loops com rótulos
+        // rotulos externos e internos
+
+        externo:
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(i == 1 && j == 1){
+                    break externo;
+                }
+
+                System.out.println("i" + i + " , j " + j);
+            }
+        }
+
+
+
     }
 }
