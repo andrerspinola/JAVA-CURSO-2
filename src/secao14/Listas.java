b46e883f0aad0866590d7cbecaef16cc058aa1c1
andre <andrecastor828@gmail.com>
2026-05-11 15:17:17 -0300

    Matrizes

--- src/secao14/Listas.java
+++ src/secao14/Listas.java
@@ -218,8 +218,40 @@ public class Listas {
         System.out.println(Arrays.toString(arrayOriginal));
         System.out.println(Arrays.toString(arrayClone));
 
+        // 9 - arrays 2d
+        // arrays de arrays [[1,2] , [2,3]]
+        // array [0][1]
 
+        int[][] matriz = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        System.out.println(matriz[1][2]);
+
+        // criando array 2d vazio e preenchendo
+        int [][] tabela = new int[3][3];
+        tabela[0][0] = 10;
+        tabela[0][0] = 10;
+        tabela[0][0] = 10;
+        for(int[] linha : tabela){
+            System.out.println(Arrays.toString(linha));
+        }
+        int [][] grade = new int[4][5];
+        for(int m =0; m < grade.length; m++){
+            for (int n = 0; n< grade[m].length;n++){
+                grade[m][n] = m*n;
+
+            }
+        }
+        for(int[] linha : grade){
+            System.out.println(Arrays.toString(linha));
+        }
 
     }
+
+
+
+
 
 }
