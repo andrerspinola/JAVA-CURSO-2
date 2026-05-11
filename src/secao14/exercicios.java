fbb3e15a51640d9f7120411e6fd4e7b13a26d0cf
andre <andrecastor828@gmail.com>
2026-05-11 15:30:37 -0300

    exercicio 1

---
+++ src/secao14/exercicios.java
@@ -0,0 +1,17 @@
+package secao14;
+import java.util.Arrays;
+
+public class exercicios {
+    static void main() {
+
+        // Exercicio 1
+        int[] arrayOriginal = {1, 2, 3};
+        int[] arrayInvertido = new int[arrayOriginal.length];
+
+        for(int i = 0; i < arrayOriginal.length; i++){
+            arrayInvertido[i] = arrayOriginal[arrayOriginal.length -1 -i];
+        }
+        System.out.println(Arrays.toString(arrayOriginal));
+        System.out.println(Arrays.toString(arrayInvertido));
+    }
+}
