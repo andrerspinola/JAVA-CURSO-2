3140327b0904a8353f4e81bec2a507bca312f4eb
andre <andrecastor828@gmail.com>
2026-05-14 10:45:28 -0300

    Ordenação de Arrays

---
+++ src/secao14/AvancandoArrays.java
@@ -0,0 +1,49 @@
+package secao14;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class AvancandoArrays {
+    static void main() {
+
+        // 1 - Ordenação
+
+        // Ordenação arrays de inteiros
+        int[] numbers = {5, 6, 1, 4, 2, 22, 1};
+
+        // Modifica o array original
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+
+        // Ordenacao com comparator
+        String [] names = {"João" , "Bob" , "Pedro", "Marcos"};
+        Arrays.sort(names);
+
+        System.out.println(Arrays.toString(names));
+
+        // Comparator.reverseOrder() to maior para o menor ou de Z até A
+        Arrays.sort(names, Comparator.reverseOrder());
+        System.out.println(Arrays.toString(names));
+
+        // Ordenação de Matriz
+
+        int[][] matriz ={
+                {4,5,1},
+                {3,8,9},
+                {2,3,1}
+
+        };
+
+        // Ordenação de linhas, por um valor de Coluna
+        Arrays.sort(matriz, Comparator.comparingInt(a -> a[1]));
+
+        for(int[] linha : matriz){
+            System.out.println(Arrays.toString(linha));
+
+        }
+
+
+    }
+}
